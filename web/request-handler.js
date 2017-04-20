49fe2813d168c6feab18c377773a7de604344fbb
Hack Reactor Student <student@hackreactor.com>
2017-04-19 20:16:17 -0700

    [Updated functionality of request-handler-js. Passing index.html]

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,31 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var paths = archive.paths;
+var fs = require('fs');
+var test;
+var httpHelpers = require('./http-helpers');
+var headers = httpHelpers.headers;
 // require more modules/folders here!
 
-exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+exports.handlereq = function (req, res) {
+  if (req.method === 'GET') {
+    fs.readFile(path.join(paths.siteAssets, '/index.html'), function (err, data) {
+      
+      if (err) {
+        return console.error(err);
+      }
+
+      console.log('Asynchronous read: ' + data.toString());
+      res.writeHead(200, headers);
+      req.on('data', function(chunk) {
+        data += chunk;
+      });
+
+      req.on('end', function() {
+        res.end(data);
+      });
+
+    });
+  }
+  //res.end(archive.paths.list);
 };
